5a8a7741dfbbf27dac9b62dfa491992a2fd62bff
Tom Lee <tlee418@g.ucla.edu>
2015-08-25 16:43:53 -0700

    user can now chat

--- fun-road-trip.js
+++ fun-road-trip.js
@@ -1,23 +1,73 @@
+
+
+Chats = new Mongo.Collection('chats');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  Meteor.subscribe("chats");  // This is to subscribe the server side data
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.body.helpers({
+    chats: function() {
+        return Chats.find();
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.events({
+    'submit .new-chat': function(event) {
+      event.preventDefault();
+      var title = event.target.title.value;
+      if (title[0].value !== null){
+        Meteor.call("addChat", title);
+      }
+      event.target.title.value = "";
     }
   });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+  Meteor.publish("chats", function(){
+    return Chats.find({  //This is Mongo DB query
+      $or: [
+        { private: {$ne: true} },  //$ne means not equal in MongoDB
+        { owner: this.userId }
+      ]
+    });
+  });
 }
+
+
+Meteor.methods({
+  addChat: function(title){
+    Chats.insert(
+    {
+        title: title,
+        createdAt: new Date(),
+        owner: Meteor.userId()
+    }
+    );
+  },
+
+  updateChat: function(id, checked) {
+    Chats.update(id, {$set: {checked: checked}});
+  },
+
+  deleteChat: function(id){
+    Chats.remove(id);
+  },
+
+  setPrivate: function(id, private){
+    var task = Chats.findOne(id);
+
+    if (task.owner !== Meteor.userId()){
+      throw new Meteor.Error('not-authorized');
+    }
+    Chats.update(id, {$set: {private: private}});
+  }
+});
